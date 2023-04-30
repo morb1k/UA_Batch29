3f6f53a6860caceda157105b8bbe623fac364766
morb1k <93850423+morb1k@users.noreply.github.com>
2023-04-30 17:24:08 -0400

    Updated Class

--- src/Nick/Claass.java
+++ src/Nick/Claass.java
@@ -3,5 +3,7 @@ package Nick;
 public class Claass {
     public static void main(String[] args) {
         System.out.println(" Hello mf");
+
+        System.out.println("Не хочу смотреть, хочу участвовать!");
     }
 }
